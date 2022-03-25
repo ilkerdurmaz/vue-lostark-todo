93b4d3c1b6a4f10644af5e5299732d5b7b80d60c
ilkerdurmaz <ilker.prime@gmail.com>
2022-03-25 17:32:56 +0300

    Account-wide task list added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
-  <div class="container">
-    <HeaderComp></HeaderComp>
+  <div>
+    <header-comp></header-comp>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HeaderComp from "@/components/shared/HeaderComp";
+import headerComp from "@/components/shared/HeaderComp";
+
 
 export default {
   components: {
-    HeaderComp,
+    headerComp
   },
+  beforeCreate() {
+
+    if (localStorage.length === 0) {
+
+      localStorage.setItem("charList", JSON.stringify([]));
+      localStorage.setItem("accWideTasks", JSON.stringify([]));
+
+    }
+  }
+
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,52 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
+  state: {
+    charList: [],
+    accWideTasks: []
+  },
+  getters: {
+    getCharList(state) {
+      return state.charList;
+
+    },
+    getAccWideTasks(state) {
+      return state.accWideTasks;
+    }
+  },
+  mutations: {
+    addCharList(state, newChar) {
+      state.charList.push(newChar);
+      localStorage.setItem("charList", JSON.stringify(state.charList));
+    },
+    addAccWideTasks(state, newTask) {
+      state.accWideTasks.push(newTask);
+      localStorage.setItem("accWideTasks", JSON.stringify(state.accWideTasks));
+    },
+    loadData(state) {
+      state.charList = JSON.parse(localStorage.getItem("charList"));
+      state.accWideTasks = JSON.parse(localStorage.getItem("accWideTasks"));
+    },
+    deleteChar(state, char) {
+      if (confirm("CONFIRM DELETE CHARACTER")) {
+        let index = state.charList.indexOf(char);
+        if (index > -1) {
+          state.charList.splice(index, 1);
+          localStorage.setItem("charList", JSON.stringify(state.charList));
+        }
+      }
+    },
+    updateCharTasks(state, char, index) {
+      state.charList[index] = char;
+      localStorage.setItem("charList", JSON.stringify(state.charList));
+    },
+    updateAccWideTasks(state, index) {
+      if (index > -1) {
+        state.accWideTasks.splice(index, 1);
+        localStorage.setItem("accWideTasks", JSON.stringify(state.accWideTasks));
+      }
+    }
+  },
   actions: {},
-  modules: {},
+  modules: {}
 });
